68d2ccc5f34d7295452b23835725d4581321636f
Saevar <sveppir@gmail.com>
2014-11-12 03:12:29 +0100

    All files but report 2

---
+++ assignment2_data/Final_folder/pipe_pocsag.mm
@@ -0,0 +1,54 @@
+CFG: Quit 0
+CFG: Warn 0
+CFG: Debug 0 
+RES: IssueWidth 4
+RES: MemLoad 1
+RES: MemStore 1
+RES: MemPft 1
+
+RES: Alu.0 4
+RES: IssueWidth.0 4
+RES: Memory.0 1
+RES: Mpy.0 1
+RES: CopySrc.0 1
+RES: CopyDst.0 1
+
+
+DEL: AluR.0 0		
+DEL: Alu.0 0 		
+DEL: CmpBr.0 1 
+DEL: CmpGr.0 0 
+DEL: Select.0 0 
+DEL: Multiply.0 1 
+DEL: Load.0 1 
+DEL: LoadLr.0 2
+DEL: Store.0 0 		 	
+DEL: Pft.0 0 
+DEL: CpGrBr.0 1 
+DEL: CpBrGr.0 0 
+DEL: CpGrLr.0 1 
+DEL: CpLrGr.0 0 
+DEL: Spill.0 0 
+DEL: Restore.0 1 
+DEL: RestoreLr.0 2
+
+DEL: AluR.1 0		
+DEL: Alu.1 0 		
+DEL: CmpBr.1 1 
+DEL: CmpGr.1 0 
+DEL: Select.1 0 
+DEL: Multiply.1 1 
+DEL: Load.1 1 
+DEL: LoadLr.1 2
+DEL: Store.1 0 		 	
+DEL: Pft.1 0 
+DEL: CpGrBr.1 1 
+DEL: CpBrGr.1 0 
+DEL: CpGrLr.1 1 
+DEL: CpLrGr.1 0 
+DEL: Spill.1 0 
+DEL: Restore.1 1 
+DEL: RestoreLr.1 2
+
+REG: $r0 62 
+REG: $b0 8
